5a611fcfcbc0f9db7b6c034bb0ba98eb1006529f
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2023-05-07 14:06:30 -0500

    actualizacion 07/05/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountriesModule } from './modules/countries/countries.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    CountriesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
